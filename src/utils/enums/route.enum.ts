3ad9b6661cc5e421597d2941a98f49ff70e61e10
Draym <kevin.draym@gmail.com>
2023-04-21 13:42:43 +0800

    task: add project create

--- src/utils/enums/route.enum.ts
+++ src/utils/enums/route.enum.ts
@@ -1,6 +1,7 @@
 enum Route {
     HOME = "/",
     APP_PROJECTS = "/projects",
+    APP_PROJECTS_Create = "/projects/new",
     APP_COLLECTIONS = "/collections",
     APP_COLLECTIONS_Details = "/collections/:address",
     APP_CONTRACT_DEPLOY = "/contract/deploy",
